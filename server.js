71aee7b119fdf2f566d50f7db7d1968c13fcee2f
Narender009 <161072326+Narender009@users.noreply.github.com>
2025-07-31 12:23:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(limiter);
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'https://www.kamallegalfintax.com/',
+  origin: process.env.CLIENT_URL || 'https://localhost:5173',
   credentials: true
 }));
 
